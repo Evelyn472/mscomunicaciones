550575ae3440aa21e09e240abce4c904707eadc8
Evelyn <eve.mora@duocuc.cl>
2026-05-19 16:09:07 -0400

    Microservicio terminado

--- src/main/java/com/colegio/mscomunicaciones/client/CursoClient.java
+++ src/main/java/com/colegio/mscomunicaciones/client/CursoClient.java
@@ -4,9 +4,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ms-cursos", url = "http://localhost:8082/api/curso")
+@FeignClient(name = "ms-cursos", url = "http://localhost:8082")
 public interface CursoClient {
 
-    @GetMapping("/{id}")
+    @GetMapping("/api/v1/cursos/{id}") 
     Object obtenerPorId(@PathVariable("id") Long id);
 }
